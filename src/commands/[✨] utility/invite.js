ef21eb5604d08a2f0c354ddd383e1558dfa6a5a8
Mira <75848@siswa.unimas.my>
2021-08-12 15:31:44 +0800

    fix typo

--- src/commands/[✨] utility/invite.js
+++ src/commands/[✨] utility/invite.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'send a bot invite link',
 	category: '[✨] utility',
 	run: async (client, interaction) => {
-		interaction.reply({ content: 'Here is the invite link:\n<https://discord.com/api/oauth2/authorize?client_id=873899675135123516&permissions=0&scope=bot%20applications.commands>', ephemral: true });
+		interaction.reply({ content: 'Here is the invite link:\n<https://discord.com/api/oauth2/authorize?client_id=873899675135123516&permissions=0&scope=bot%20applications.commands>', ephemeral: true });
 	},
 };
