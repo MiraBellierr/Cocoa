64c45c0168425478af562665c925925d0514d9fa
Mira <75848@siswa.unimas.my>
2021-08-12 15:30:32 +0800

    add invite.js

---
+++ src/commands/[✨] utility/invite.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'invite',
+	description: 'send a bot invite link',
+	category: '[✨] utility',
+	run: async (client, interaction) => {
+		interaction.reply({ content: 'Here is the invite link:\n<https://discord.com/api/oauth2/authorize?client_id=873899675135123516&permissions=0&scope=bot%20applications.commands>', ephemral: true });
+	},
+};
