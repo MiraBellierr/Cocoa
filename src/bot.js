5533205a87f5255b6d2dfc1574210ebc1c31a2de
MiraBellierr <75848@siswa.unimas.my>
2023-03-16 21:28:02 +0800

    remove mongoose

--- src/bot.js
+++ src/bot.js
@@ -1,40 +1,32 @@
-const { Client, Collection } = require('discord.js');
-const fs = require('fs');
-const mongoose = require('mongoose');
-
+const { Client, Collection } = require("discord.js");
+const fs = require("fs");
 
 const client = new Client({
-	allowedMentions: { parse: ['users'] },
+	allowedMentions: { parse: ["users"] },
 	intents: [
-		'DIRECT_MESSAGES',
-		'DIRECT_MESSAGE_REACTIONS',
-		'DIRECT_MESSAGE_TYPING',
-		'GUILDS',
-		'GUILD_BANS',
-		'GUILD_EMOJIS_AND_STICKERS',
-		'GUILD_INTEGRATIONS',
-		'GUILD_INVITES',
-		'GUILD_MEMBERS',
-		'GUILD_MESSAGES',
-		'GUILD_MESSAGE_REACTIONS',
-		'GUILD_MESSAGE_TYPING',
-		'GUILD_PRESENCES',
-		'GUILD_VOICE_STATES',
-		'GUILD_WEBHOOKS',
+		"DIRECT_MESSAGES",
+		"DIRECT_MESSAGE_REACTIONS",
+		"DIRECT_MESSAGE_TYPING",
+		"GUILDS",
+		"GUILD_BANS",
+		"GUILD_EMOJIS_AND_STICKERS",
+		"GUILD_INTEGRATIONS",
+		"GUILD_INVITES",
+		"GUILD_MEMBERS",
+		"GUILD_MESSAGES",
+		"GUILD_MESSAGE_REACTIONS",
+		"GUILD_MESSAGE_TYPING",
+		"GUILD_PRESENCES",
+		"GUILD_VOICE_STATES",
+		"GUILD_WEBHOOKS",
 	],
 });
 
 client.commands = new Collection();
-client.categories = fs.readdirSync('./src/commands/');
+client.categories = fs.readdirSync("./src/commands/");
 
-['command', 'event'].forEach(handler => {
+["command", "event"].forEach((handler) => {
 	require(`./handlers/${handler}`)(client);
 });
-
-const connect = async () => await mongoose.connect('mongodb://localhost/cocoa', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-connect();
 
 client.login(process.env.TOKEN);
